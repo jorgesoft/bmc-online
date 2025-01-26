d697229e9075b8a73c55dc6f284f99c7e2255369
adiverxd <adiverxd@gmail.com>
2025-01-26 03:54:07 +0000

    Change bootstrap theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootswatch/dist/cosmo/bootstrap.min.css';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootswatch/dist/sandstone/bootstrap.min.css';
+//import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import './index.css'
